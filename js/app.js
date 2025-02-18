418b00d5cba60eaffb37ebda2096799c5d0810cf
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-18 14:59:18 -0800

    Add initial game state function. Add board and message elements. Add square click event listener

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const states = ["win", "lose", "tie"];
 /*---------------------------- Variables (state) ----------------------------*/
-
+let isX = true; //is player x or o?
 /*------------------------ Cached Element References ------------------------*/
-
+const boardElement = document.querySelector(".board");
+const messageElement = document.querySelector("#message");
 /*-------------------------------- Functions --------------------------------*/
+function handleClick() {
+  //todo: check if player is x or o and place corresponding symbol on board
+}
 
+function renderGameInitial() {
+  for (let sqr of boardElement.children) {
+    sqr.textContent = "";
+  }
+  messageElement.innerText = "Begin Game. Player X's Turn";
+}
 /*----------------------------- Event Listeners -----------------------------*/
+document.querySelectorAll(".sqr").forEach(function (sqr) {
+  sqr.addEventListener("click", handleClick);
+});
