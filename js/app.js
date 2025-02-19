e6f7e18697982d0dadb2e72904d7c79c258ba9bb
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-18 22:41:02 -0800

    Generate 2D board and incorporate game logic

--- js/app.js
+++ js/app.js
@@ -19,6 +19,91 @@ function handleClick() {
   isX = !isX;
 
   console.log(`handleClick.successful: Player ${xOrO}`);
+
+  const board = computeBoard(boardElement);
+  const winner = getWinner(board);
+  console.log(`winner`, winner);
+  if (typeof winner === "string") {
+    messageElement.innerText = `🎉 Player ${winner} wins! 🎉`;
+  }
+}
+
+function computeBoard(boardElement) {
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  for (let i = 0; i < boardElement.children.length; i++) {
+    const square = boardElement.children[i];
+    board[Math.floor(i / 3)][i % 3] = square.innerText;
+  }
+
+  console.log(board);
+  return board;
+}
+
+function isValidWinner(s) {
+  return s === "X" || s === "O";
+}
+
+/**
+ * @param {string[][]} board
+ * @returns {"X" | "O" | undefined}
+ */
+function getWinner(board) {
+  // check rows
+  for (let row = 0; row < 3; row++) {
+    const firstCellInRow = board[row][0];
+    if (!isValidWinner(firstCellInRow)) {
+      continue;
+    }
+    for (let col = 0; col < 3; col++) {
+      if (board[row][col] !== firstCellInRow) {
+        break;
+      }
+
+      if (col === 2) {
+        return firstCellInRow;
+      }
+    }
+  }
+
+  // check columns
+  for (let col = 0; col < 3; col++) {
+    const firstCellInCol = board[0][col];
+    if (!isValidWinner(firstCellInCol)) {
+      continue;
+    }
+    for (let row = 0; row < 3; row++) {
+      if (board[row][col] !== firstCellInCol) {
+        break;
+      }
+
+      if (row === 2) {
+        return firstCellInCol;
+      }
+    }
+  }
+
+  // check diagonals
+  if (
+    isValidWinner(board[0][0]) &&
+    board[0][0] === board[1][1] &&
+    board[1][1] === board[2][2]
+  ) {
+    return board[0][0];
+  }
+
+  if (
+    isValidWinner(board[0][2]) &&
+    board[0][2] === board[1][1] &&
+    board[1][1] === board[2][0]
+  ) {
+    return board[0][2];
+  }
+
+  return undefined;
 }
 
 function renderGameInitial() {
